ff916d8defc7b67cac58ab1c15dd617c09b2b446
Pablo Moreira Mora <pablo.moreiramora@gmail.com>
2012-06-19 13:17:08 -0300

    Specs for Controllers

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe HomeController do
+  describe "Loading current site" do
+    before :each do
+      @current_site = mock_model(Site)
 
+      Site.stub!(:first).and_return(@current_site)
+    end
+
+    it "finds current site" do
+      Site.should_receive(:first).and_return(@current_site)
+      get :show
+      assigns[:current_site].should == @current_site
+    end
+  end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  describe "Inherited Resources" do
+    before :each do
+      @projects = mock_model(Project)
 
+      controller.stub!(:current_site).and_return(@site)
+      @site.stub!(:projects).and_return(@projects)
+    end
+
+    it "Begin Association" do
+      controller.stub!(:current_site).and_return(@site)
+      @site.stub!(:projects).and_return(@projects)
+      get :index
+      response.should be_success
+    end
+  end
 end
