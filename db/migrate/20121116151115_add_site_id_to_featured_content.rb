fd38bca286f05c44ac5ce78812e3c6380744097e
Rafael Alejandro Lizardo Celiz <rafael@insignia4u.com>
2012-11-16 16:56:20 -0300

    [Delete Migration]

--- db/migrate/20121116151115_add_site_id_to_featured_content.rb
+++
@@ -1,9 +0,0 @@
-class AddSiteIdToFeaturedContent < ActiveRecord::Migration
-  def self.up
-    add_column :featured_contents, :site_id, :integer
-  end
-
-  def self.down
-    remove_column :featured_contents, :site_id
-  end
-end
