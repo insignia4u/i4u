f4e2ea8864e1e6d3ae017978f2063a1e06139ebb
Augusto Pedraza <augusto@insignia4u.com>
2012-11-19 11:00:08 -0300

    [Modified Show Featured Content on CMS] -  Updated show for featured content item on CMS.

--- app/admin/featured_contents.rb
+++ app/admin/featured_contents.rb
@@ -9,5 +9,20 @@ ActiveAdmin.register FeaturedContent do
     end
 
     f.buttons
-  end  
+  end
+
+  show do
+    attributes_table do
+      row "Site" do
+        featured_content.site.name
+      end
+      row :name
+      row :body  do
+        textilize(featured_content.body)
+      end
+      row :image do
+        image_tag(featured_content.image.url(:cms_thumb))
+      end
+    end
+  end
 end
