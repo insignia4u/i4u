293b71e1bf2d961c22a7bc75379d0ec982875297
Augusto Pedraza <augusto@insignia4u.com>
2012-11-30 14:48:05 -0300

    [Issue #14: Add the users admin management]

---
+++ app/admin/admin_users.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register AdminUser do
+  filter :email
+
+  index do
+    column :email
+    column :current_sign_in_at
+    column :last_sign_in_at
+    column :sign_in_count
+    default_actions
+  end
+
+  show do
+    attributes_table do
+      row :sign_in_count
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.buttons
+  end
+end
