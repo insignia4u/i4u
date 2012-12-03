c9be4bb74b7697bab08361706e395973185d5757
Augusto Pedraza <augusto@insignia4u.com>
2012-12-03 18:54:20 -0300

    [Issue #33: add sorting to staff ActiveAdmin page]
     - Add sort by position to index page to ActiveAdmin's person.
     - Add migration and modified Person model to add the position field.
     - Add callback to Person, Technology and Tool models to create record with default position.
     - Modified the javascript to management the sorting of rows on the views index to ActiveAdmin.

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -4,30 +4,41 @@
 
 $(document).ready(function(){
   makeTableSortable("#button-reorder-technologies", '#technologies');
-  makeTableSortable("#button-reorder-tools", '#tools');
+  makeTableSortable("#button-reorder-tools",        '#tools');
+  makeTableSortable("#button-reorder-people",       '#people');
 })
 
 function makeTableSortable(dataSortUrlElementId, tableId ){
+  if ( '#' + $('table').attr('id') !== tableId) {
+    return;
+  }
+  
   var $reorder = $(dataSortUrlElementId);
   $reorder.css('display', 'none');
 
   var $table = $(tableId + ' tbody');
 
-  $table.find('tr').find('td:first').css('cursor', 'move');
+  var nroOfHeader = $(tableId + 'th').length;
+
+  //Selecciona todos los td, excepto los de la última columna.
+  var $handle = $table.find('tr td').not(':nth-child(' + nroOfHeader + ')');
+
+  $handle.css('cursor', 'move');
 
   $table.sortable({
-    items: 'tr',
-    axis: "y",
-    cursor: "move",
-    handle: $table.find('tr').find('td:first'),
+    items:  'tr',
+    axis:   'y',
+    cursor: 'move',
+    handle: $handle,
     helper: function(e, ui) {
+      //Mantiene el ancho de la fila completa.
       ui.children().each(function() {
         $(this).width($(this).width());
       });
       return ui;
     },
 
-    stop: function(event, ui) {
+    stop:  function(event, ui) {
       $.ajax({
         url:     $reorder.attr('data-sort-url'), type: 'post',
         data:    $(this).sortable('serialize'),
