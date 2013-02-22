decdb74303c96fd350a5b0922a6f181c99d3e4f5
Juan Maria Martinez Arce <juan@insignia4u.com>
2013-02-22 11:49:54 -0300

    [Refactoring] Remove global config loader.

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,5 @@
+MAIL_CHIMP = YAML.load_file("#{Rails.root}/config/gibbon.yml")[Rails.env].symbolize_keys!
+
 Gibbon.api_key           = MAIL_CHIMP[:api_key]
 Gibbon.timeout           = 15
 Gibbon.throws_exceptions = false
