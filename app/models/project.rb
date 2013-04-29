e63c23abc239a134ed4a3771362a38c9414a3277
dan1d <danielfromarg@gmail.com>
2013-04-29 10:16:46 -0300

    [New site integration - Refactor models][Daniel]

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ require 'valid_formats'
 
 class Project < ActiveRecord::Base
   extend FriendlyId
+  include Project::Validations
+  include Project::Attachments
+  include Project::Featuring
+  include Project::Finders
 
   belongs_to :site
 
@@ -14,11 +18,6 @@ class Project < ActiveRecord::Base
   attr_accessible :site_id, :description, :ended_at, :extended_description,
     :name, :started_at, :summary, :url, :image, :featured_image,
     :technology_ids, :tool_ids, :site, :technologies, :tools, :highlighted
-
-  include Project::Validations
-  include Project::Attachments
-  include Project::Featuring
-  include Project::Finders
 
   friendly_id :name, use: [:slugged, :history]
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,14 +1,14 @@
 class Service < ActiveRecord::Base
+
   belongs_to :site
 
   attr_accessible :site_id, :summary, :title, :image
 
+  has_attached_file :image, styles: {thumb: "281x292#", cms_thumb: "100x100#"}
+
   validates :site, presence: true
   validates :title, presence: true
   validates :summary, presence: true
-
-  has_attached_file :image, styles: {thumb: "281x292#", cms_thumb: "100x100#"}
-
   validates_attachment :image, presence: true,
     content_type: { content_type: ['image/jpeg', 'image/png'] }
 end
