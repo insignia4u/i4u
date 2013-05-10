1e26101f1c4334975470fb739762d90e6532ce82
dan1d <danielfromarg@gmail.com>
2013-05-10 15:12:36 -0300

    Delete blank spaces in AdminUser specs.

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -12,15 +12,12 @@ describe AdminUser do
 
     context "Password Structure" do
       it { should respond_to(:encrypted_password) }
-
       it { should respond_to(:reset_password_token) }
     end
 
     context "Audit Trail" do
       it { should respond_to(:remember_created_at) }
-
       it { should respond_to(:sign_in_count) }
-
       it { should respond_to(:current_sign_in_at) }
       it { should respond_to(:last_sign_in_at) }
       it { should respond_to(:current_sign_in_ip) }
