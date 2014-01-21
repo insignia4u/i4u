7addd803037f9d3230b6ab9b7d5c1f2649b8707f
Ezekiel <ezerivadeneiral@gmail.com>
2014-01-21 13:01:09 -0300

    [Carrers Page]
      - Values models with integration
      - Spec

---
+++ app/admin/values.rb
@@ -0,0 +1,43 @@
+ActiveAdmin.register Values do
+  filter :site
+
+  index do
+    column ("Site") { |value| value.site_name }
+    column ("Title"), sortable: :title do |value|
+      value.title
+    end
+    column ("Subtitle"), sortable: :subtitle do |value|
+      value.subtitle
+    end
+    column ("Image") do |value|
+      if value.image.present?
+        image_tag value.image, size: '50x50'
+      else
+        ''
+      end
+    end
+
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Edit Values" do
+      f.input :site_id, :label => "Site",
+              :as => :select, :collection => Hash[Site.all.map{|s| [s.name, s.id]}]
+      f.input :title
+      f.input :subtitle
+      f.input :description
+      f.input :image, hint: ( (f.object.new_record?)? "" : f.object.image.url ),
+              as: :file
+    end
+    f.buttons
+  end
+
+  controller do
+    def resource_params
+      return [] if request.get?
+      [ params.require(:values)
+        .permit(:site_id, :title, :subtitle, :description, :image) ]
+    end
+  end
+end
---
+++ spec/models/values_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Values do
+  describe "Associations" do
+    it { should belong_to(:site) }
+  end
+  describe "Attributes accesibility" do
+    it { should respond_to(:title) }
+    it { should respond_to(:description) }
+    it { should respond_to(:subtitle) }
+    it { should respond_to(:image) }
+    it { should respond_to(:site) }
+  end
+
+  describe "Validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:site) }
+    it { should validate_uniqueness_of(:title) }
+  end
+
+end
