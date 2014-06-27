537898b30134e5fd5b7a90c117cad26b74ee9cf0
Diego Flores <diegoflores1991@gmail.com>
2014-06-27 11:42:03 -0300

    assets config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,7 +18,7 @@ I4u::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   config.assets.precompile += %w{
