b5d7a40b0907051f25efdf356f243a6979b6eb91
Carlos Kozuszko <ckozus@gmail.com>
2015-03-20 10:36:48 -0300

    Mejoras al populator

--- lib/populator/blog.rb
+++ lib/populator/blog.rb
@@ -5,19 +5,30 @@ module Populator
     end
 
     def populate
-      Article.populate 30 do |article|
-        article.title = Faker::Name.title.titleize
-        article.subtitle = Populator.words(0..20).titleize
-        article.author = Faker::Name.name
-        article.description = Populator.words(30..130).titleize
-        article.summary = Faker::Lorem.paragraphs.join('\n')
-        article.content = Faker::Lorem.paragraphs.join('\n')
-        article.publication_date = 2.years.ago..Time.now
-        article.publication_state = 1
-        article.image_file_name = 'default.png'
-        article.site_id = Site.all.sample.id
-        article.created_at = Time.now
+      30.times do 
+        Article.create! title:  Faker::Name.title.titleize,
+          subtitle: Populator.words(0..20).titleize,
+          author: Faker::Name.name,
+          description: Populator.words(30..130).titleize,
+          summary: Faker::Lorem.paragraphs.join('\n'),
+          content: Faker::Lorem.paragraphs.join('\n'),
+          publication_date: rand(2.years).ago,
+          publication_state: 1,
+          image: image,
+          site_id: Site.all.sample.id,
+          created_at: Time.now
       end
     end
+
+    def image
+      @images ||= Dir[File.join(Rails.root, 'public', 'populate_images', 'articles')]
+      if @images.empty?
+        puts "WARNING: Put images on ROOT/public/populate_images/articles to populate articles with proper images."
+        nil
+      else
+        File.open(@images.sample)
+      end
+    end
+
   end
 end
