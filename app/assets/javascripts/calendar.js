d84a5abaa23b58b844855fd45471bad7cb7296df
Ery Lopez <lopez1erika@gmail.com>
2015-06-02 11:22:02 -0300

    Deletes unnecesary code

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -3,15 +3,13 @@ $( document ).ready( function(){
   $('.wrapper-calendar').each(function(){
     var month = $(this).find('.hidden-dates').html().trim().split(" ");
 
-    console.log("-----------------");
-    console.log(month);
     var dates = [];
     for (var i = 0; i < month.length; i++ ){
       var json = { title: 'Clase ' + x , start: month[i], allDay: false}
       dates.push(json);
       x++;
     }
-    console.log(dates)
+
     $(this).fullCalendar({
         header: {
             left: 'prev,next today',
@@ -22,8 +20,7 @@ $( document ).ready( function(){
         events: dates,
         timeFormat: '(H:mm)'
       });
-    console.log("<<<<<<<<<<<<<<<<<<<")
-      console.log(dates[0])
+
       $(this).fullCalendar('gotoDate', dates[0].start);
   });
 });
